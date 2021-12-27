dc526a61554245064c850c8200ed9869260ed0c1
Loovelllyy <sedykh.k.n@gmail.com>
2021-12-27 15:49:14 +0300

    updates

---
+++ src/components/buttonAdd/buttonAdd.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import styles from './butonAdd.css'
+
+class ButtonAdd extends Component<any, any> {
+
+    render() {
+        return (
+            <div className={styles}>
+                +
+            </div>
+        )
+    }
+}
+
+export default ButtonAdd;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ButtonAdd from "./components/buttonAdd/buttonAdd.tsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ButtonAdd />
   </React.StrictMode>,
   document.getElementById('root')
 );
