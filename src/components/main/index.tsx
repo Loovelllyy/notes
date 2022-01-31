6c59ba638cf8070504008b1eaac95f4f23c78954
Looovelllyy <sedykh.k.n@gmail.com>
2022-01-31 17:07:47 +0300

    Update index.tsx

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -5,22 +5,27 @@ import Note from "../note";
 import Navbar from "../navbar";
 import style from './style.module.css'
 
-type TData = {id: number | string,  title: string, text: string}[];
+type TData = {id: number,  title: string, text: string}[];
 type initial_data = {notes: TData};
-// const arrNotes: TData = [];
+
+// let testNote = {id: 2, title: 'Welcome!', text: 'Hello! This is your first note. Let\'s make many new notes with important information for you and our service help you to save it all'};
 
 class Main extends Component<unknown, initial_data>{
     constructor(props: unknown) {
         super(props);
-        this.state = {'notes': [{  id: 1, title: 'Welcome!', text: 'Hello! This is your first note. Let\'s make many new notes with important information for you and our service help you to save it all'}]};
+        this.state = {'notes': [{id: 1, title: 'Welcome!', text: 'Hello! This is your first note. Let\'s make many new notes with important information for you and our service help you to save it all'}]};
     }
 
     componentDidMount() {
         console.log('mounted');
     }
 
-    // async func
-    getNotes(): TData {
+    componentDidUpdate() {
+        console.log('updates');
+    }
+
+
+    getNotes(): TData { // async func
         let notes: TData = [];
         for(let i = 0; i < localStorage.length; i++) {
             notes.push(JSON.parse(localStorage.getItem(i.toString()) || '{}'));
@@ -28,45 +33,24 @@ class Main extends Component<unknown, initial_data>{
         return notes;
     }
 
-    // countId (): number {
-    //     // this.setState({id: this.state.id + 1})
-    //     return 1;
-    // }
-
-    // toNote(arr: TData): JSX.Element[] {
-    //     let newArr: JSX.Element[] = [];
-    //     arr.map(el => {
-    //         let handle = this.countId()
-    //         newArr.push(<Note key={handle} title={el.title} text={el.text}   />)
-    //     })
-    //     // console.log(this.state.notes)
-    //     return newArr;
-    //     // return <Note key={obj.id} title={obj.title} text={obj.text}   />
-    // }
-
-    changeState(): void {
-        // this.setState({id: this.state.notes.push({1: this.state.id! + 1})});
-        console.log(this.state.notes)
-    }
-
-    componentDidUpdate() {
-        console.log('updates');
-    }
-
     createNote(): void{
         console.log('create note');
         // this.setState({notes: this.getNotes()});
         // console.log(this.state.notes)
+    }
 
+    deleteNote(id: number): void{
+        this.state.notes.map(el => {
+            if(el.id === id) {
+                localStorage.removeItem(id.toString());
+            }
+            this.setState({notes: this.getNotes()})
+        })
+    }
 
-        // // let test1: JSX.Element = <Note title={'dslf'} text={'slfslfk'} />;
-        //
-        // let test = localStorage.getItem('1')
-        // // @ts-ignore
-        // let arrTest: TData = JSON.parse(test);
-        // console.log(arrTest)
-        // let arr = [...this.state.notes, ...this.toNote(arrTest)];
-        // this.setState({notes: arr})
+    changeState(): void {
+        // this.setState({id: this.state.notes.push({1: this.state.id! + 1})});
+        console.log(this.state.notes)
     }
 
     render() {
